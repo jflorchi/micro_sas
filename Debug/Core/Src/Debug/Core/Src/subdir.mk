bb880378582aaaafb9664ae24799b114a3598aa6
jflorchi <jordan.florchinger@alumni.uleth.ca>
2022-06-03 12:59:32 -0600

    init commit

---
+++ Debug/Core/Src/Debug/Core/Src/subdir.mk
@@ -0,0 +1,17 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../Core/Src/Debug/Core/Src/main.o \
+../Core/Src/Debug/Core/Src/stm32f0xx_hal_msp.o \
+../Core/Src/Debug/Core/Src/stm32f0xx_it.o \
+../Core/Src/Debug/Core/Src/syscalls.o \
+../Core/Src/Debug/Core/Src/sysmem.o \
+../Core/Src/Debug/Core/Src/system_stm32f0xx.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
